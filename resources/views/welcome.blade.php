b688f9212c296afd64aec79e1bc1ce9d04cc51a0
awiipp <sayidariipp@gmail.com>
2024-08-21 09:04:26 +0700

    add models and migrations.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('section')
-    <h1 class="text-4xl font-bold text-center text-black/80">your page here</h1>
+    <h1 class="text-4xl font-bold text-center text-black/80">Tes</h1>
 @endsection
